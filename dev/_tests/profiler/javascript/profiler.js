eb45a73879b4828d3edcccb38592f3bc678c2074
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2010-10-05 15:56:57 -0400

    initial profiler page

---
+++ dev/_tests/profiler/javascript/profiler.js
@@ -0,0 +1,14 @@
+var url = "";
+var profile = function(){
+    url = $("#url").val();
+    $("#pframe").attr("src", url);
+    return false;
+};
+
+$(document).ready(function() {
+    var qs = new Querystring();
+    url = qs.get("url", "");
+    $("#url").val(url);
+    $("#pframe").attr("src", url);
+    $("#pbutton").click(function() { profile(); });
+});
